99a3ec5d9a6d3caab9d13701ce32fde9d71117fd
Gitesh sawariya <gitesh@scriptknack.com>
2024-09-20 16:27:41 +0530

    read me init

--- server/src/config/route.config.ts
+++ server/src/config/route.config.ts
@@ -1,30 +1,31 @@
 import JWTPassport from "passport-jwt";
-// import UserModel from "../models/User/index";
+import UserModel from "../models/user/index";
 
 // Extract necessary methods from passport-jwt module
 const JWTStratergy = JWTPassport.Strategy;
 const ExtractJWT = JWTPassport.ExtractJwt;
 
 // Define options for JWT strategy
-const options = {
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.JWTSECRET || " ",
+const options =
+{
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWTSECRET || " ",
 };
 
 // Export a function that initializes and configures the JWT strategy for Passport
 export default (passport: any) => {
-  passport.use(
-    new JWTStratergy(options, async (jwt_payload: any, done: any) => {
-      try {
-        // const user = await UserModel.findById(jwt_payload.user);
-        // if (!user) return done(null, false);
+    passport.use(new JWTStratergy(options, async (jwt_payload: any, done: any) => {
+        try {
+            const user = await UserModel.findById(jwt_payload.user);
+            if (!user) return done(null, false);
 
-        // If user exists, authentication succeeds and user information is passed to the next middleware
-        // return done(null, user);
-        return done(null);
-      } catch (error) {
-        throw new Error();
-      }
-    })
-  );
-};
+            
+            // If user exists, authentication succeeds and user information is passed to the next middleware
+            return done(null, user);
+
+        } catch (error) {
+
+            throw new Error();
+        }
+    }))
+}
